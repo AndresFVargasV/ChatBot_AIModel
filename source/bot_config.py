bb91741a9c8ee35f719a524b1cd631504cdd0752
AndresFVargasV <afvasquez@uninorte.edu.co>
2024-01-23 21:28:49 -0500

    bug fixed

--- source/bot_config.py
+++ source/bot_config.py
@@ -2,18 +2,17 @@ from telegram import Update
 from telegram.ext import CallbackContext, Application, MessageHandler, CommandHandler, filters
 import json
 
+with open("config.json", 'r') as f:
+    config = json.load(f)
 
+token = config.get('token')
 
 # Define a function that will be called when the /start command is issued.
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Hello! I am a bot. Please talk to me!')  
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('Hello! I am a bot. Please talk to me!')  
 
 
 def main() -> None:
-    with open('config.json', 'r') as f:
-        config = json.load(f)
-
-    token = config.get('token')
 
     # Create the Telegram Application, using the token.
     app = Application.builder().token(token).build()
@@ -21,6 +20,9 @@ def main() -> None:
     # Add a command handler to the Application. This command handler will be Start.
     start_handler = CommandHandler('start', start)
     app.add_handler(start_handler)
-    
+
+    # Start the polling
+    app.run_polling()
+
 if __name__ == '__main__':
     main()
